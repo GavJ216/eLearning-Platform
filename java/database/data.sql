8e604d85f86dac7da0517ced1f4538f4d4ec9b69
Brett Balika <brettbalika@gmail.com>
2022-12-06 13:04:04 -0500

    Update 1: login/register/update database/coursedao start

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,6 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO course (course_name,course_description, difficulty, cost) VALUES ('sample_course','this is a course','very difficult', 100);
+
 COMMIT TRANSACTION;
