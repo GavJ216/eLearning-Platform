13af119c5d614c67abc9f296808ee8232e83b4e7
Gavin Javorsky <gjavorsky21@gmail.com>
2022-12-08 10:40:48 -0500

    Tested data

--- java/database/data.sql
+++ java/database/data.sql
@@ -4,6 +4,13 @@ INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULi
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
 INSERT INTO course (course_name,course_description, difficulty, cost) VALUES ('sample_course','this is a course','very difficult', 100);
-INSERT INTO course (course_name,course_description, difficulty, cost) VALUES ('sample_course2','this is another course','easy peasy lemon squeezy', 150);
+INSERT INTO course (course_name,course_description, difficulty, cost) VALUES ('sample_course2','this is another course','easy peasy', 150);
+
+UPDATE users
+SET role = 'ROLE_ADMIN'
+WHERE username = 'GUH'
+
+SELECT *
+FROM users
 
 COMMIT TRANSACTION;
