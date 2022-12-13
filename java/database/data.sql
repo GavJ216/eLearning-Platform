11169495ba3386e83879b32b36f74e42c5db8eb4
Brett Balika <brettbalika@gmail.com>
2022-12-13 09:23:11 -0500

    some database changes to lessons and quizzes

--- java/database/data.sql
+++ java/database/data.sql
@@ -6,7 +6,7 @@ INSERT INTO users (first_name,last_name,username,password_hash,role) VALUES ('us
 
 INSERT INTO course (course_name,course_description, difficulty, cost) VALUES ('Cyber Security','This course gives an in-depth view on best cyber security practices. The course introduces the concepts and understanding of the field of computer security and how it relates to other areas of information technology. Topics include security threats, hardening systems, securing networks, cryptography and organizational security policies.','Beginner', 0);
 
-INSERT INTO course (course_name,course_description, difficulty, cost) VALUES ('sample_course2','Ka ecabadic si ene sucuto neri; lehal tin netotu! Laya iciemu yo reyut remel ika upiep taqiese de! Irieli tefol sasiral. Abupage lig mihaboy orisenem asileli jeceri apo. Pahiza avidogis maso sinowos meliemo colanu supelu meg ticagag ici. Alil rakucir cim nasivieh nuwape ta neqade, tadil suwirie adicoriy fec. Ca nesi tu, asofi wize nuroh penitob nasi! Dibe pucat no lomasi sesov gahie nonago. Ludu yo inodimir','Intermediate', 0);
+INSERT INTO course (course_name,course_description, difficulty, cost) VALUES ('Creating Safe Passwords','Ka ecabadic si ene sucuto neri; lehal tin netotu! Laya iciemu yo reyut remel ika upiep taqiese de! Irieli tefol sasiral. Abupage lig mihaboy orisenem asileli jeceri apo. Pahiza avidogis maso sinowos meliemo colanu supelu meg ticagag ici. Alil rakucir cim nasivieh nuwape ta neqade, tadil suwirie adicoriy fec. Ca nesi tu, asofi wize nuroh penitob nasi! Dibe pucat no lomasi sesov gahie nonago. Ludu yo inodimir','Intermediate', 0);
 
 COMMIT TRANSACTION;
 
@@ -17,10 +17,10 @@ BEGIN TRANSACTION;
 INSERT INTO users_course (user_id,course_id) VALUES (3,1),(2,1);
 INSERT INTO users_course (user_id,course_id) VALUES (3,2);
 
-INSERT INTO lesson (course_id,lesson_name,lesson_description) VALUES (1,'imma teach you a lesson','this is a lesson about teaching you about lessons');
-INSERT INTO lesson (course_id,lesson_name,lesson_description) VALUES (2,'less son','more daughter');
+INSERT INTO lesson (course_id,lesson_name,lesson_description) VALUES (1,'Cybersecurity','This lesson will teach you everything about Cybersecurity.');
+INSERT INTO lesson (course_id,lesson_name,lesson_description) VALUES (2,'Password Security','This lesson will teach you everything about making a good password.');
 
-INSERT INTO quiz (lesson_id,quiz_name,quiz_description) VALUES (1,'sample course Quiz #1','this quiz is a doozy');
-INSERT INTO quiz (lesson_id,quiz_name,quiz_description) VALUES (2,'sample course2 Quiz #1','this quiz is a piece of cake');
+INSERT INTO quiz (lesson_id,quiz_name,quiz_description) VALUES (1,'Cybersecurity Quiz #1','Test your knowledge of Cybersecurity!');
+INSERT INTO quiz (lesson_id,quiz_name,quiz_description) VALUES (2,'Password Tips Quiz #1','How good are your passwords?');
 
 COMMIT TRANSACTION;
